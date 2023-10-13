e8ac3d5d0326abc2f20c7c3656bbaddb9afc0166
dhaniald <gtiwari95@gmail.com>
2023-10-13 09:16:07 +0530

    api work done

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,18 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//1.Import area
+
+//2.Defenation area
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  //function serialzation
+  //object.method().method(argument)
+                      //{Property:value} is a js object
+  res.status(200).json({
+    Country: `My country is-${req.query.state}`,
+    State:"Uttar pradesh",
+    city1:`is- ${req.query.city1}`,
+      //obj.property.property
+    testkey:req.query.state
+  })
 }
+
+//3. Export area 
